22dfb145b7516b0233087a1ea74185b5734c5fc1
Sergey <super.super.spore.com97@gmail.com>
2023-03-12 16:31:41 +0900

    new functions in code of file 'Task_1'

--- Tasks_Lesson_8/Task_1.py
+++ Tasks_Lesson_8/Task_1.py
@@ -69,6 +69,54 @@ def import_contacts():
                 f2.write(contact)
         print("Данные успешно импортированы в телефонную книгу")
 
+# Определяем функцию для изменения данных в телефонной книге
+def change_contact():
+    surname = input("Введите фамилию контакта для изменения данных: ")
+    name = input("Введите имя контакта для изменения данных: ")
+    with open("Tasks_Lesson_8/phonebook.txt", "r", encoding='utf-8') as f:
+        contacts = f.readlines()
+    found = False
+    with open("Tasks_Lesson_8/phonebook.txt", "w", encoding='utf-8') as f:
+        for contact in contacts:
+            if surname.lower() in contact.lower() and name.lower() in contact.lower():
+                found = True
+                new_surname = input(f"Введите новую фамилию ({contact.strip().split(',')[0]}): ")
+                if not new_surname:
+                    new_surname = contact.strip().split(',')[0]
+                new_name = input(f"Введите новое имя ({contact.strip().split(',')[1]}): ")
+                if not new_name:
+                    new_name = contact.strip().split(',')[1]
+                new_patronymic = input(f"Введите новое отчество ({contact.strip().split(',')[2]}): ")
+                if not new_patronymic:
+                    new_patronymic = contact.strip().split(',')[2]
+                new_phone_number = input(f"Введите новый номер телефона ({contact.strip().split(',')[3]}): ")
+                if not new_phone_number:
+                    new_phone_number = contact.strip().split(',')[3]
+                f.writelines(f"{new_surname},{new_name},{new_patronymic},{new_phone_number}\n")
+                print("Контакт успешно изменен")
+            else:
+                f.writelines(contact)
+    if not found:
+        print("Контакт не найден")
+
+# Определяем функцию для удаления данных из телефонной книги
+def delete_contact():
+    surname = input("Введите фамилию контакта для удаления: ")
+    name = input("Введите имя контакта для удаления: ")
+    with open("Tasks_Lesson_8/phonebook.txt", "r", encoding='utf-8') as f:
+        contacts = f.readlines()
+    found = False
+    with open("Tasks_Lesson_8/phonebook.txt", "w", encoding='utf-8') as f:
+        for contact in contacts:
+            if surname.lower() in contact.lower() and name.lower() in contact.lower():
+                found = True
+                print(f"Контакт {contact.strip()} успешно удален")
+            else:
+                f.writelines(contact)
+    if not found:
+        print("Контакт не найден")
+
+
 # Определяем основную функцию, которая обеспечивает взаимодействие с пользователем
 def main():
     while True:
@@ -78,7 +126,9 @@ def main():
         print("3. Найти контакт по характеристике")
         print("4. Экспортировать телефонную книгу в файл")
         print("5. Импортировать данные в телефонную книгу из файла")
-        print("6. Выход")
+        print("6. Удалить данные из телефонной книги")
+        print("7. Изменить данные из телефонной книги")
+        print("8. Выход")
         choice = input("Выберите действие: ")
         if choice == "1":
             add_contact()
@@ -91,6 +141,10 @@ def main():
         elif choice == "5":
             import_contacts()
         elif choice == "6":
+            delete_contact()
+        elif choice == "7":
+            change_contact()
+        elif choice == "8":
             break
         else:
             print("Неверный ввод. Попробуйте еще раз.")
